84d887bc08dc259a873cb3454383a9584ae0d4ed
Pedro Cipriano <pedro.cipriano@outlook.com>
2014-01-05 21:50:20 +0000

    Added qt-solutions submodule

---
+++ Submodules/qtservice.cmake
@@ -0,0 +1,28 @@
+set(SOURCES
+        Submodules/qt-solutions/qtservice/src/qtservice.cpp
+        )
+
+set(HEADERS
+        Submodules/qt-solutions/qtservice/src/qtservice.h
+        Submodules/qt-solutions/qtservice/src/qtservice_p.h
+        )
+    
+if(WIN32)
+    set(EXTRA_INCLUDES
+            Submodules/qt-solutions/qtservice/src/qtservice_win.cpp
+        )
+    set(EXTRA_LIBS
+            user32
+        )
+elseif(UNIX)
+    set(EXTRA_INCLUDES
+            Submodules/qt-solutions/qtservice/src/qtunixsocket.h
+            Submodules/qt-solutions/qtservice/src/qtunixserversocket.h
+            Submodules/qt-solutions/qtservice/src/qtservice_unix.cpp
+            Submodules/qt-solutions/qtservice/src/qtunixsocket.cpp
+            Submodules/qt-solutions/qtservice/src/qtunixserversocket.cpp
+        )
+endif()
+
+add_library(QtSolutions_Service ${SOURCES} ${HEADERS} ${EXTRA_INCLUDES})
+target_link_libraries(QtSolutions_Service ${EXTRA_LIBS} ${QT_LIBRARIES})

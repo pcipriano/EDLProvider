c3a4723c8d352082e6781e3eef0742475acb20d5
Pedro Cipriano <pedro.cipriano@outlook.com>
2014-01-13 21:46:10 +0000

    Implemented application creation

--- Core/src/EDLProviderService.cpp
+++ Core/src/EDLProviderService.cpp
@@ -25,7 +25,11 @@ void EDLProviderService::stop()
 
 void EDLProviderService::createApplication(int& argc, char** argv)
 {
+    QtService::createApplication(argc, argv);
 
+    application()->setApplicationName(edlprovider::info::PROJECT_USER_NAME);
+    application()->setApplicationVersion(edlprovider::info::EDLPROVIDER_VERSION);
+    application()->setObjectName(edlprovider::info::PROJECT_NAME);
 }
 
 int EDLProviderService::executeApplication()
